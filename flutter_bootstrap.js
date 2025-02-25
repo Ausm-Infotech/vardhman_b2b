0c7bb0fed14674afceacea6f99c386ed7e4859a5
arjuntare <arjuntare@users.noreply.github.com>
2025-02-25 08:02:27 +0000

    deploy: a49e413592db06ea91ec394614b4ae5d44316ff7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "307454258"
+    serviceWorkerVersion: "1586179346"
   }
 });
