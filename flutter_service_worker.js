fabfe1b6254db99a77bb1210e70ab83ad4a17c2e
arjuntare <arjuntare@users.noreply.github.com>
2025-03-05 05:48:49 +0000

    deploy: 983d37f0e6ac6f91ad17e3a8a889f947f2bb256d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/AssetManifest.json": "f075a9fa2beef8363667811088e822b2",
 "version.json": "b76b5fae1b745de148ef5a71b11dfee1",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"main.dart.js": "16eeef365f298f5c415b0d4587dc75df",
+"main.dart.js": "4d2c13d4b4b7e1593d563f925d070a9e",
 "drift_worker.js": "24184a64355f79fdde67276d6f9076dc",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -47,7 +47,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "8c34c8ddaacc0c2c4d9e9249aeb97558",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "c814fe141a5eccf4868051997d2736a4"};
+"flutter_bootstrap.js": "1cf34d5fdedcb19b4c6ba5c6cd9fa222"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
